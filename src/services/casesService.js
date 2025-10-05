9d438aee259d5ad50cec7a7e96d10285e9e3b05b
ManishYelam <manishyelam12e@gmail.com>
2025-10-05 13:14:05 +0530

    a

--- src/services/casesService.js
+++ src/services/casesService.js
@@ -2,7 +2,7 @@ import axios from "./axiosInstance";
 
 const base_url = 'http://localhost:5000/api'
 
-export const getAllCases = () => axios.get("/cases");
+export const getAllCases = (data) => axios.post(`${base_url}/case/`, data);
 
 export const getCaseById = (id) => axios.get(`/cases/${id}`);
 
