0722644915b4eabeb1320caf2f4ef8d19dc2bea8
ManishYelam <manishyelam12e@gmail.com>
2025-10-10 17:04:44 +0530

    hj

--- src/services/documentsService.js
+++ src/services/documentsService.js
@@ -1,7 +1,5 @@
 import axiosInstance from "./axiosInstance";
 
-const base_url = 'http://localhost:5000/api'
-
 export const getAllDocuments = () => axiosInstance.get("/documents");
 
 export const uploadDocument = (formData) => axiosInstance.post("/documents", formData, { headers: { "Content-Type": "multipart/form-data" }, });
