bce0b7fb337b386f0bb763f95f61fce1296ac670
Manish-Yelam <manish.ovtpl@gmail.com>
2025-10-18 21:36:14 +0530

    d

--- src/services/applicationService.js
+++ src/services/applicationService.js
@@ -5,9 +5,6 @@ const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api"
 export const saveApplicationData = async (applicationData) => {
   try {
     const response = await axios.post(`${baseURL}/users/save-application`, applicationData);
-    console.log("-------------------------------------------------", applicationData);
-    console.log("-------------------------------------------------", response);
-
     return response;
   } catch (error) {
     console.error("Error saving application:", error);
@@ -15,10 +12,9 @@ export const saveApplicationData = async (applicationData) => {
   }
 };
 
-
 export const checkExistsEmail = (email) => axios.post(`${baseURL}/users/email`, { email });
 
-export const userApplicant = (id) => axiosInstance.get(`${baseURL}/users/${id}`);
+export const userApplicant = (id) => axios.get(`${baseURL}/users/${id}`);
 
 export const updateUserApplicant = (id, data) => axios.put(`${baseURL}/users/${id}`, data);
 
